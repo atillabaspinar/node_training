7adc5e3293d3569be9c775e5e01669542f6d3ba8
Atilla Baspinar <atilla.baspinar@acrontum.de>
2020-06-19 17:20:07 +0200

    convert to ts

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.header);
+  console.log(req.headers);
+  res.send('hello');
+  // process.exit(); // exit the event loop
 });
 
 server.listen(9001);
---
+++ dist/app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log(req.headers);
+    res.send('hello');
+    // process.exit(); // exit the event loop
+});
+server.listen(9001);
+//# sourceMappingURL=app.js.map
