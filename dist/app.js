46ffef3b2ab695f98bf33c2eee30a6b38f9e5453
Atilla Baspinar <atilla.baspinar@acrontum.de>
2020-06-19 18:49:09 +0200

    add routing

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.send('hello');
-    // process.exit(); // exit the event loop
-});
-server.listen(9001);
-//# sourceMappingURL=app.js.map
