988082ff715cf050595f72e0d18298254f759aca
Atilla Baspinar <atilla.baspinar@acrontum.de>
2020-06-19 23:53:49 +0200

    express router

--- app.ts
+++ app.ts
@@ -1,7 +1,32 @@
-const http = require('http');
-import { routes } from './routes';
 
-const server = http.createServer(routes);
+import express from 'express';
+import bodyParser from 'body-parser';
 
-server.listen(9001);
+const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// this will match /test/1 but not with /test123 !!!
+app.use('/add-category', (req, res, next) => {
+  res.send(
+    `<html><body><form method="POST" action="/category" >
+    <input type="text"name="product">    
+    <button type="submit">add category</button>
+    </form></body></html>`);
+});
+
+// app.post, get etc matches exactly. so /category/1 does not match to this!
+// app.use on the other hand, matchs /category/1 also
+app.post('/category', (req, res, next) => {
+  res.send(
+    `<html><body>
+    <h1>product added</h1>
+    </body></html>`);
+});
+
+// this matches /segment /segmenta /segment/a 
+app.use('/', (req, res, next) => {
+  res.send('every other request');
+})
+
+app.listen(9001);
